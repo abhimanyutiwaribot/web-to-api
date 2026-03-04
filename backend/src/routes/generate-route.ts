a09d3a818c375d2366b95e379fe28e211fb91738
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-03-04 22:39:44 +0530

    changes in routes

--- backend/src/routes/generate-route.ts
+++ backend/src/routes/generate-route.ts
@@ -1,11 +1,22 @@
 import express from "express";
-import { isModuleBody } from "typescript";
+import { generateAPI } from "../controllers/generate";
 
 const generateRouter = express.Router();
 
-generateRouter.post('/generate', (req, res) => {
-  //  const { url } = req.body;
+generateRouter.post('/generate', async (req, res) => {
+  const { url } = req.body;
   
+  if(!url){
+    return res.status(400).json({
+      message: "URL is required"
+    })
+  }
+
+  const data = await generateAPI(url as string)
+  
+  return res.json({
+    data
+  });
 
 })
 
