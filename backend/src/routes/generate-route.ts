4ba1bb855912b2dc57639317279b91055a53e068
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-03-04 20:54:53 +0530

    add: routes

---
+++ backend/src/routes/generate-route.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { isModuleBody } from "typescript";
+
+const generateRouter = express.Router();
+
+generateRouter.post('/generate', (req, res) => {
+  //  const { url } = req.body;
+  
+
+})
+
+
+export default generateRouter;
