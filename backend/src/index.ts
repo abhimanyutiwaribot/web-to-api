9992a30369132d79d92d8936f3cd045bb053392b
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-03-05 22:26:05 +0530

    new: added routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import generateRouter from "./routes/generate-route";
+import projectRouter from "./routes/project-route";
 
 const app = express();
 const port = 3000;
@@ -10,7 +11,8 @@ app.get('/', (req, res) => {
   res.send("Hello from / route")
 });
 
-app.use('/api/x', generateRouter)
+app.use('/api/x', generateRouter);
+app.use('/api/v1/projects', projectRouter);
 
 app.listen(port, () => {
   console.log("Server is running on: " + port)
